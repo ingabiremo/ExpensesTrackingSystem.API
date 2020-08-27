9682ed04407eab2f1fc7a1f060d10f4b11bfe754
Moise Ingabire <ingabiremo@yahoo.fr>
2020-08-26 23:25:12 -0400

    Add entities

---
+++ Entities/Categories.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpensesTrackingSystem.API.Entities
+{
+    public class Categories
+    {
+    }
+}
